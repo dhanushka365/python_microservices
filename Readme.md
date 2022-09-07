b2ed9dcd375af095b3d572708051c8b97af51317
uduwela96 <puduwel@mitrai.com>
2022-09-07 13:00:21 +0530

    host name and ip added

--- Readme.md
+++ Readme.md
@@ -9,3 +9,12 @@ pip list
 
 #install Flask
 pip install Flask
+
+#create the requirements.txt file
+pip freeze > requirements.txt
+
+#install the requirements from txt file
+python -m pip install -r requirements.txt
+
+#start minikube cluster
+strat minikube
--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
-from flask import Flask, jsonify
+import socket
+from unicodedata import name
+from flask import Flask, jsonify, render_template
 
 app=Flask(__name__)
+
+#Function to fetch hostname and Ip
+def fetchDetails():
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    print("Hostname:",host_name)
+    print("Ip:",host_ip)
+    return str(host_name), str(host_ip)
 
 @app.route('/')
 def home():
@@ -11,6 +21,12 @@ def health():
     return jsonify(
         status="UP"
     )
+    
+@app.route("/details")
+def details():
+    host_name, host_ip = fetchDetails()
+    return render_template('index.html',host_name=host_name,host_ip=host_ip)
+    
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
